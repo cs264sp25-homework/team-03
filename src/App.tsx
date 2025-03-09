326558b4754e8803a47012107162eb906c62fe24
ashley-yuanyuan <yliu443@jh.edu>
2025-03-09 18:01:34 -0400

    created hello world for frontend and convex for backend

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Hello World!</h1>
+    </div>
+  )
+}
+
+export default App;
