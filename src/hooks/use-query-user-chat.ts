40b46d9e902602b8794f057ba35ccb84ba30756a
ashley-yuanyuan <yliu443@jh.edu>
2025-04-06 17:03:46 -0400

    fixed initial load bug, ready to merge with main

--- src/hooks/use-query-user-chat.ts
+++ src/hooks/use-query-user-chat.ts
@@ -3,7 +3,12 @@ import { api } from "../../convex/_generated/api";
 
 
 export function useQueryUserChat() {
-  
-  const chat = useSessionQuery(api.chats.getUserChat);
-  return chat;
+  try {
+    const chat = useSessionQuery(api.chats.getUserChat);
+    return chat;
+  } catch (error) {
+    // If there's an auth error or any other error, return null
+    // This will allow the app to continue working and retry later
+    return null;
+  }
 } 
